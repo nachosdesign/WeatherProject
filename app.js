59ae1ffbfe970dde8d519c8cc28be5c10224f695
Nacho Allendesalazar <iallende@rebelliusamishl.home>
2021-02-06 20:32:38 +0000

    day2.2

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
 const { json } = require("express");
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=ee8eaf75f184e4c9292e2848991eaa73&units=metric"
-    
-    https.get(url, function(response){
-        console.log(response.statusCode);
-    
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data)
-            const temp = weatherData.main.temp
-            const weatherDescription = weatherData.weather[0].description
-            const icon = weatherData.weather[0].icon
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-
-            res.write("<h1>The temperature in London is " + temp + " degrees Celcius.</h1>");
-            res.write("<p>The weather decription is " + weatherDescription + " in London</p>");
-            res.write("<img src=" + imageURL + ">");
-            res.send()
-        })
-    })
+    res.sendFile(__dirname + "/index.html")
 })
+
+app.post("/", function(req, res){
+    console.log(req.body.cityName);
+    console.log("Post request recived");
+})
+
+
+
+// const query = "London";
+// const apiKey = "ee8eaf75f184e4c9292e2848991eaa73";
+// const unit = "metric"
+// const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units="+ unit + ""
+
+// https.get(url, function(response){
+//     console.log(response.statusCode);
+
+//     response.on("data", function(data){
+//         const weatherData = JSON.parse(data)
+//         const temp = weatherData.main.temp
+//         const weatherDescription = weatherData.weather[0].description
+//         const icon = weatherData.weather[0].icon
+//         const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+//         res.write("<h1>The temperature in London is " + temp + " degrees Celcius.</h1>");
+//         res.write("<p>The weather decription is " + weatherDescription + " in London</p>");
+//         res.write("<img src=" + imageURL + ">");
+//         res.send()
+//     })
+// })
 
 
 
